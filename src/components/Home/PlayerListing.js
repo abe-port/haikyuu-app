b3fdc4ec5648c1f0908e2a9a6df1bde3432faa28
Abraham Portugal <abrportu@WKMZTB742B98.global.publicisgroupe.net>
2022-01-06 11:06:42 -0800

    Listing Style WIP

--- src/components/Home/PlayerListing.js
+++ src/components/Home/PlayerListing.js
@@ -2,16 +2,70 @@ import { useState } from 'react';
 import CourtSrc from '../../assets/img/court.png';
 import styled from 'styled-components';
 
+const PlayerListingHeader = styled.h3`
+font-size:16px;
+margin-bottom: 0;
+@media(min-width: 768px) {
+    font-size:24px;
+   }
+`;
+
+const UnorderedList = styled.ul`
+list-style-type:none;
+padding-inline-start: 0px;
+`;
+
+const ListItem = styled.li`
+border-bottom:1px solid #cccccc;
+padding:15px;
+display: flex;
+font-size:14px;
+line-height:16px
+@media(min-width: 768px) {
+    font-size:18px;
+    line-height:22px
+   }
+${props => props.bolded && `
+font-weight:800
+`};
+&:nth-child(even){
+    background:#cccccc;
+}
+`;
+
+const Name = styled.div`
+width:33%;
+`;
+
+const Number = styled.div`
+width:33%;
+`;
+
+const Position = styled.div`
+width:33%;
+`;
+
 function PlayerListing(props) {
 
     return (
         <div>
-            Players ({props.players.length}):
-            <div>
+            <PlayerListingHeader>
+                Players ({props.players.length}):
+            </PlayerListingHeader>
+            <UnorderedList>
+                <ListItem bolded>
+                    <Name className="name">NAME</Name>
+                    <Number className="number">NUMBER</Number>
+                    <Position className="position">POSITION NUMBER</Position>
+                </ListItem>
                 {props.players.map((player, index) => (
-                    <li key={index}> {player.first_name} {player.last_name}</li>
+                    <ListItem key={index}>
+                        <Name className="name">{player.first_name} {player.last_name}</Name>
+                        <Number className="number">{player.number}</Number>
+                        <Position className="position">{player.position}</Position>
+                    </ListItem>
                 ))}
-            </div>
+            </UnorderedList>
         </div>
     );
 }
